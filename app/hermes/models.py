de93620f7585d9918a2ef80091ccc37bae3dc66a
simontunmore <stunmore@bink.com>
2021-06-23 19:16:06 +0100

    WIP

--- app/hermes/models.py
+++ app/hermes/models.py
@@ -13,6 +13,7 @@ class User(Base):
     __table__ = Table('user', metadata, autoload=True)
     profile = relationship("UserDetail", backref="user", uselist=False)   # uselist = False sets one to one relation
     scheme_account_user_associations = relationship("SchemeAccountUserAssociation", backref="user")
+    payment_account_user_assoc = relationship("PaymentAccountUserAssociation", backref="user")
 
 
 class UserDetail(Base):
@@ -72,5 +73,15 @@ class SchemeAccountCredentialAnswer(Base):
     __table__ = Table('scheme_schemeaccountcredentialanswer', metadata, autoload=True)
     scheme_account_assoc = relationship("SchemeAccount", backref="scheme_account_credential_answer")
     scheme_credential_question_assoc = relationship("SchemeCredentialQuestion", backref="scheme_account_credential_answer")
+
+
+class PaymentAccountUserAssociation(Base):
+    __table__ = Table('ubiquity_paymentcardaccountentry', metadata, autoload=True)
+
+
+class PaymentAccount(Base):
+    __table__ = Table('payment_card_paymentcardaccount', metadata, autoload=True)
+    payment_account_user_assoc = relationship("PaymentAccountUserAssociation", backref="payment_account")
+
 
 
--- app/resources/payment_accounts.py
+++ app/resources/payment_accounts.py
@@ -1,7 +1,6 @@
 import falcon
 from .base_resource import Base
-from app.hermes.models import SchemeAccountUserAssociation, SchemeAccount, Scheme, SchemeChannelAssociation, \
-    SchemeCredentialQuestion, SchemeAccountCredentialAnswer, Channel
+from app.hermes.models import User, Channel, PaymentAccountUserAssociation, PaymentAccount
 from sqlalchemy import insert
 from app.api.auth import get_authenticated_user, get_authenticated_channel
 from app.messaging.sender import send_message_to_hermes
@@ -14,4 +13,55 @@ class PaymentAccounts(Base):
         user_id = get_authenticated_user(req)
         channel = get_authenticated_channel(req)
         post_data = req.media
+
+        data = {}
+        request_fields = ['expiry_month',
+                          'expiry_year',
+                          'name_on_card',
+                          'issuer', 'token',
+                          'last_four_digits',
+                          'first_six_digits',
+                          'fingerprint',
+                          'provider',
+                          'type',
+                          'country',
+                          'currency_code']
+
+        compare_fields = ['expiry_month', 'expiry_year', 'name_on_card', ]
+
+        try:
+            for field in request_fields:
+                data[field] = post_data[field]
+        except(KeyError, AttributeError):
+            raise falcon.HTTPBadRequest('Missing parameters')
+
+        existing_accounts = self.session.query(PaymentAccount, User)\
+            .select_from(PaymentAccount)\
+            .join(PaymentAccountUserAssociation)\
+            .join(User)\
+            .filter(PaymentAccount.fingerprint == data['fingerprint'])\
+            .all()
+
+        linked_users = []
+        compare_details = {}
+
+        for account in existing_accounts:
+            print(account.PaymentAccount.fingerprint)
+            linked_users.append(account.User.id)
+            compare_details['expiry_month']
+
+
+        if len(existing_accounts) > 0:
+            if user_id in linked_users:
+                if data['expiry_month']
+            else:
+                print("ACCOUNT EXISTS IN ANOTHER WALLET - LINK THIS USER")
+
+
+        else:
+            print("THIS IS A NEW ACCOUNT")
+
+
+
+
 
