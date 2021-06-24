ffaff5565e7a39cf9c0e5d57211a04310922e54f
simontunmore <stunmore@bink.com>
2021-06-24 09:49:25 +0100

    Added functionality for existing payment accounts

--- app/resources/payment_accounts.py
+++ app/resources/payment_accounts.py
@@ -1,7 +1,7 @@
 import falcon
 from .base_resource import Base
 from app.hermes.models import User, Channel, PaymentAccountUserAssociation, PaymentAccount
-from sqlalchemy import insert
+from sqlalchemy import insert, update
 from app.api.auth import get_authenticated_user, get_authenticated_channel
 from app.messaging.sender import send_message_to_hermes
 from datetime import datetime
@@ -27,8 +27,6 @@ class PaymentAccounts(Base):
                           'country',
                           'currency_code']
 
-        compare_fields = ['expiry_month', 'expiry_year', 'name_on_card', ]
-
         try:
             for field in request_fields:
                 data[field] = post_data[field]
@@ -43,24 +41,71 @@ class PaymentAccounts(Base):
             .all()
 
         linked_users = []
-        compare_details = {}
+        compare_fields = {}
 
-        for account in existing_accounts:
-            print(account.PaymentAccount.fingerprint)
-            linked_users.append(account.User.id)
-            compare_details['expiry_month']
+        if len(existing_accounts) > 1:
+            print("TOO MANY ACCOUNTS WITH THIS FINGERPRINT!")
 
+        elif len(existing_accounts) == 1:
 
-        if len(existing_accounts) > 0:
+            existing_payment_account = existing_accounts[0].PaymentAccount
+
+            print(existing_accounts[0].PaymentAccount.fingerprint)
+            linked_users.append(existing_accounts[0].User.id)
+            compare_fields['expiry_month'] = existing_payment_account.expiry_month
+            compare_fields['expiry_year'] = existing_payment_account.expiry_year
+            compare_fields['name_on_card'] = existing_payment_account.name_on_card
+
             if user_id in linked_users:
-                if data['expiry_month']
+                if self.fields_match_existing(data, compare_fields):
+                    print("RETURN EXISTING ACCOUNT DETAILS")
+                    details = self.payment_account_info_to_dict(existing_payment_account)
+                    resp.body = details
+                    resp.status = falcon.HTTP_200
+                else:
+                    print("UPDATE EXISTING ACCOUNT DETAILS WITH NEW INFORMATION")
+
+                    self.session.update(PaymentAccount)\
+                        .where(PaymentAccount.id == existing_payment_account.id)\
+                        .values(expiry_month=existing_payment_account.expiry_month,
+                                expiry_year=existing_payment_account.expiry_year,
+                                name_on_card=existing_payment_account.name_on_card)
+
+                    self.session.commit()
+                    details = self.payment_account_info_to_dict(existing_payment_account)
+                    resp.body = details
+                    resp.status = falcon.HTTP_200
+
             else:
                 print("ACCOUNT EXISTS IN ANOTHER WALLET - LINK THIS USER")
-
+                self.session.insert()
 
         else:
             print("THIS IS A NEW ACCOUNT")
 
+    @staticmethod
+    def fields_match_existing(self, data, compare_details):
+
+        fields_alike = True
+
+        for key, value in compare_details:
+            if data[key] != compare_details[key]:
+                fields_alike = False
+
+        return fields_alike
+
+    @staticmethod
+    def payment_account_info_to_dict(self, existing_payment_account:PaymentAccount):
+
+        details = {"expiry_month": existing_payment_account.expiry_month,
+                   "expiry_year": existing_payment_account,
+                   "name_on_card": existing_payment_account.name_on_card,
+                   "issuer": existing_payment_account.issuer_id,
+                   "id": existing_payment_account.id,
+                   "status": existing_payment_account.status
+                   }
+
+        return details
 
 
 
