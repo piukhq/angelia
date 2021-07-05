28e5216168911f6b607efd06c4832e7ef216e48b
Demetris Williams <dwilliams@bink.com>
2021-07-05 13:46:20 +0100

    remove whitespace

--- qa_tests/environments/__init__.py
+++ qa_tests/environments/__init__.py
@@ -7,7 +7,6 @@ import os
 import re
 
 
-
 def read_env():
     """Pulled from Honcho code with minor updates, reads local default
     environment variables from a .env file located in the project root
