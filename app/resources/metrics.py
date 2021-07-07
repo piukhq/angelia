ac7a5ab973cacbcc1c9fed464d10bf7115fff415
Terry Cain <tcain@bink.com>
2021-07-07 17:30:06 +0100

    Fixed url's and prometheus

---
+++ app/resources/metrics.py
@@ -0,0 +1,25 @@
+from os import getenv
+
+import falcon
+
+from app.api.auth import NoAuth
+from prometheus_client import CONTENT_TYPE_LATEST, REGISTRY, CollectorRegistry, generate_latest, multiprocess
+
+from .base_resource import Base
+
+
+class Metrics(Base):
+
+    auth_class = NoAuth
+
+    def on_get(self, req: falcon.Request, resp: falcon.Response, **kwargs) -> None:
+        registry = REGISTRY
+
+        if getenv("PROMETHEUS_MULTIPROC_DIR"):
+            registry = CollectorRegistry()
+            multiprocess.MultiProcessCollector(registry)
+
+        resp.text = generate_latest(registry)
+        resp.set_header("Content-Type", CONTENT_TYPE_LATEST)
+        resp.status = falcon.HTTP_200
+
