652bab3c38d46736b818edf6630ddb5645d5a36a
simontunmore <stunmore@bink.com>
2021-07-29 11:56:22 +0100

    Refined credential process and tidied up

--- app/handlers/loyalty_card.py
+++ app/handlers/loyalty_card.py
@@ -22,12 +22,14 @@ ADD_AND_AUTHORISE = 'ADD_AND_AUTH'
 JOIN = 'JOIN'
 REGISTER = 'REGISTER'
 
-credential_types = {
-    'ADD': ['add_field'],
-    'AUTH': ['add_field', 'auth_field'],
-    'JOIN': ['add_field', 'enrol_field'],
-    'REGISTER': ['add_field', 'register_field']
-}
+ADD_FIELD = 'add_field'
+AUTH_FIELD = 'auth_field'
+JOIN_FIELD = 'join_field'
+REGISTER_FIELD = 'register_field'
+
+CARD_NUMBER = 'card_number'
+BARCODE = 'barcode'
+
 
 
 @dataclass
@@ -38,8 +40,7 @@ class LoyaltyCardHandler(BaseHandler):
     add_fields: list = None
     auth_fields: list = None
     register_fields: list = None
-    main_answer: str = None
-    loyalty_card_object: SchemeAccount = None
+    key_credential: dict = None
     loyalty_plan_object: Scheme = None
     id: int = None
     valid_credentials: dict = None
@@ -70,7 +71,6 @@ class LoyaltyCardHandler(BaseHandler):
         except KeyError:
             api_logger.error('KeyError when processing cred fields')
             raise falcon.HTTPInternalServerError('An Internal Server Error Occurred')
-            pass
 
     def check_provided_credentials(self):
 
@@ -78,7 +78,7 @@ class LoyaltyCardHandler(BaseHandler):
         # question_id, question_type, and answer
 
         credential_questions = (
-            self.db_session.query(SchemeCredentialQuestion)
+            self.db_session.query(SchemeCredentialQuestion, Scheme)
                 .join(Scheme)
                 .join(SchemeChannelAssociation)
                 .join(Channel)
@@ -86,17 +86,60 @@ class LoyaltyCardHandler(BaseHandler):
                 .filter(Channel.bundle_id == self.channel_id)
                 .filter(SchemeChannelAssociation.status == 0).all())
 
+        if len(credential_questions) < 1:
+            api_logger.error('Loyalty plan does not exist, or no credential questions found')
+            raise falcon.HTTPBadRequest('This loyalty plan is not available.')
+        # todo: do we need separate errors for:
+        #  loyalty plan not available to this channel (400/401?)
+        #  loyalty plan does not exist (400/404?),
+        #  loyalty plan exists but no questions (500)?
+
+        # Store scheme object for later as will be needed for card_number/barcode regex on create
+        self.loyalty_plan_object = credential_questions[0][1]
+
+        # Checks that each given cred answer matches a question of the same type, and adds each to a central cred list.
         self.valid_credentials = {}
-        for answer in self.all_answers:
+
+        if self.add_fields:
+            self.validate_answers(credential_questions, self.add_fields, ADD_FIELD)
+        if self.auth_fields:
+            self.validate_answers(credential_questions, self.auth_fields, AUTH_FIELD)
+        if self.register_fields:
+            self.validate_answers(credential_questions, self.register_fields, REGISTER_FIELD)
+
+        # Checks that at least one manual question, scan question or one question link has been given.
+        for key, value in self.valid_credentials.items():
+            if value['key_credential']:
+                self.key_credential = value
+
+        if not self.key_credential:
+            api_logger.error('No main question (manual_question, scan_question, one_question_link found in given creds')
+            raise falcon.HTTPBadRequest('At least one manual question, scan question or one question link must be '
+                                        'provided')
+
+    def validate_answers(self, credential_questions, answer_set, credential_class):
+        for answer in answer_set:
             answer_found = False
-            for question in credential_questions:
-                if answer['credential_slug'] == question.label and \
-                        any([getattr(question, cred_type) for cred_type in credential_types[self.journey]]):
+            for question, scheme in credential_questions:
+                if answer['credential_slug'] == question.label and getattr(question, credential_class):
+
+                    # Checks if this cred is the the 'key credential'. There should only be one (this is checked later)
+                    key_credential = any([
+                        getattr(question, 'manual_question'),
+                        getattr(question, 'scan_question'),
+                        getattr(question, 'one_question_link')
+                    ])
+
+                    # For clarity:
+                    # using QUESTION when referring to only the question or credentialquestions table (i.e. receiver)
+                    # using ANSWER when referring to only the information given in the request (i.e. supplier)
+                    # using CREDENTIAL where there is an intersection, or applies to both
                     self.valid_credentials[question.label] = {
-                        "question_id": question.id,
-                        "type": question.type,
-                        "manual_question": question.manual_question,
-                        "value": answer['value']
+                        "credential_question_id": question.id,
+                        "credential_type": question.type,
+                        "credential_class": credential_class,
+                        "key_credential": key_credential,
+                        "credential_answer": answer['value'],
                     }
                     answer_found = True
                     break
@@ -104,31 +147,21 @@ class LoyaltyCardHandler(BaseHandler):
                 raise falcon.HTTPBadRequest('Invalid credentials provided')
 
     def return_existing_or_create(self):
-        card_number_question_id = None
         created = False
 
-        for key, value in self.valid_credentials.items():
-            if value['type'] == 'card_number':
-                card_number_question_id = value['question_id']
-        if card_number_question_id is None:
-            api_logger.error(f'Cannot find card_number credential question for loyalty plan {self.loyalty_plan}')
-            raise falcon.HTTPInternalServerError('An Internal Error Occurred')
+        # If the key credential (i.e. the credential
 
-        # Use the first available credential as the main credential (there should only be 1 anyway for
-        # add-only journeys.)
-        # If there are multiple creds, and one of these is marked as the main question, then use this instead.
-        self.main_answer = self.valid_credentials[list(self.valid_credentials.keys())[0]]['value']
-
-        for key, value in self.valid_credentials.items():
-            if value['manual_question']:
-                self.main_answer = self.valid_credentials[key]['value']
+        if self.key_credential['credential_type'] in [CARD_NUMBER, BARCODE]:
+            key_credential_field = self.key_credential['credential_type']
+        else:
+            key_credential_field = 'main_answer'
 
         existing_objects = (
             self.db_session.query(SchemeAccount, SchemeAccountUserAssociation, Scheme)
                 .join(SchemeAccountCredentialAnswer)
                 .join(SchemeAccountUserAssociation)
                 .join(Scheme)
-                .filter(SchemeAccount.main_answer == self.main_answer)
+                .filter(getattr(SchemeAccount, key_credential_field) == self.key_credential['credential_answer'])
                 .filter(SchemeAccount.scheme_id == self.loyalty_plan)
                 .filter(SchemeAccount.is_deleted == 'false')
                 .all())
@@ -172,9 +205,9 @@ class LoyaltyCardHandler(BaseHandler):
         barcode, card_number = None, None
 
         for key, value in self.valid_credentials.items():
-            if value['type'] == 'card_number':
-                card_number= value['value']
-            elif value['type'] == 'barcode':
+            if value['question_type'] == 'card_number':
+                card_number = value['value']
+            elif value['question_type'] == 'barcode':
                 barcode = value['value']
 
         if barcode and not card_number:
@@ -191,6 +224,7 @@ class LoyaltyCardHandler(BaseHandler):
 
         card_number, barcode = self.get_card_number_and_barcode()
 
+        # todo: look at how we write into main_answer/card_number/barcode
         loyalty_card = SchemeAccount(
             status=0,
             order=1,
@@ -235,13 +269,7 @@ class LoyaltyCardHandler(BaseHandler):
 
 # todo: case-sensitive credential answers (do we make a list of these, as in ubiquity?)
 
-# todo: handling manual question/main answer. There are some cases where this question is actually an auth field. Do
-#  we really want a user to need to include an auth field as an add field? In Ubiquity, the answer_type comes from the
-#  serializer context, which determines the type of answer required (e.g. card_number, barcode) etc. HN does have a
-#  card_number, but it's manual question is email, which is an auth field (and so theoretically shouldn't be passed in
-#  with this request for a store card)
-
-# todo: validation
+# todo: validation - regex/length etc.
 
 # todo: prevent user from adding redundant cred field types in request.
 
