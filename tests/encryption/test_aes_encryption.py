a260b5fb8a9d7e4c384240b80eff9babb4ae25b8
mmarsh <mmarsh@bink.com>
2021-09-17 13:47:08 +0100

    pep8 fix on tests

--- tests/encryption/test_aes_encryption.py
+++ tests/encryption/test_aes_encryption.py
@@ -1,5 +1,5 @@
-from app.lib.encryption import AESCipher
 from app.api.helpers.vault import AESKeyNames
+from app.lib.encryption import AESCipher
 
 
 def test_encrypt_decrypt():
@@ -8,7 +8,7 @@ def test_encrypt_decrypt():
     for value_in_clear in items:
         enc_value = cipher.encrypt(value_in_clear).decode("utf-8")
         value_decrypted = cipher.decrypt(enc_value)
-        assert(value_decrypted == value_in_clear)
+        assert value_decrypted == value_in_clear
 
 
 def test_stored_encryptions():
@@ -17,7 +17,7 @@ def test_stored_encryptions():
         "one": "upg6XeEzndQKxD0PZDUm0KUOo+20MRUl98fvchB/qV7/iggz8fNc+bvUxMpfjuwg",
         "rg1 1aa": "iF0fLjSpKFHjrbi8+syv/3Eq+OzNRKJWVLHsiR8Iz1Qhm9cREcRmLJ+iuF601jBt",
         "wefhe7¡€#∞§¶•ªº,.;'wewhf@€jhgd": "GNCiFeqs455R/S9aI4lg5LmX70jnpGCUdkqM3mTt2doikrgEPSxnKL59nIzInU19TL4if"
-                                          "DZfQ4VwT4TeBjGJiS6ddIZKs/VBzZxzZrfX5Rc=",
+        "DZfQ4VwT4TeBjGJiS6ddIZKs/VBzZxzZrfX5Rc=",
         "fgf": "rFTgJMyk6g1Rm3FlnGgusml7pu9McAmCAs82u4urohLAQ1ZkmffzzSsqLFdlKETl",
         "s": "cUsyTRqfGruZcRWY3rOPYr20wVR419282sGuewwWui009cHBohliTIMqYi44n5EC",
         "98989": "e/04jkckxrXMiYiGTei7Ilw6A67IVwYx97PLIq1X5PO8FBAFzmaUdsjmhd7BbtJ4",
@@ -25,4 +25,4 @@ def test_stored_encryptions():
     }
     for clear_item, enc_item in enc_items.items():
         decrypted = cipher.decrypt(enc_item)
-        assert(decrypted == clear_item)
+        assert decrypted == clear_item
