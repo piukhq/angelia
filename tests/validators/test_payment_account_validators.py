9a5b0d33a29afc797c1b424642ae55f32f2ab32c
MartfBink <mfranklin@bink.com>
2022-01-25 17:48:47 +0000

    style style style

--- tests/validators/test_payment_account_validators.py
+++ tests/validators/test_payment_account_validators.py
@@ -22,7 +22,7 @@ class TestReqObject:
 def resp_data():
     return {
         "id": 1,
-        "provider" : "Provider",
+        "provider": "Provider",
         "status": None,
         "name_on_card": "first last",
         "card_nickname": "nickname",
