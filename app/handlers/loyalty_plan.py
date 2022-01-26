5db7e3b8622bee19f0a7373e12ac101c341b1835
Tony Truong <ttruong@bink.com>
2022-01-26 11:45:54 +0000

    loy-1919-loyalty-plans-monitoring

--- app/handlers/loyalty_plan.py
+++ app/handlers/loyalty_plan.py
@@ -30,6 +30,7 @@ from app.hermes.models import (
 from app.lib.credentials import ANSWER_TYPE_CHOICES
 from app.lib.loyalty_plan import SchemeTier
 from app.report import api_logger
+from app.resources.metrics import loyalty_plan_get_counter
 
 
 class LoyaltyPlanJourney(str, Enum):
@@ -359,6 +360,9 @@ class LoyaltyPlanHandler(BaseHandler, BaseLoyaltyPlanHandler):
         try:
             plan_info = list(sorted_plan_information.values())[0]
         except IndexError:
+            loyalty_plan_get_counter.labels(
+                endpoint="/loyalty_plans/{loyalty_plan_id}", channel=self.channel_id, response_status=falcon.HTTP_404
+            ).inc()
             raise ResourceNotFoundError(title="Could not find this Loyalty Plan")
 
         plan_info["credentials"] = self._sort_by_attr(plan_info["credentials"])
@@ -422,6 +426,9 @@ class LoyaltyPlanHandler(BaseHandler, BaseLoyaltyPlanHandler):
         try:
             scheme_id = schemes_and_questions[0].Scheme.id
         except IndexError:
+            loyalty_plan_get_counter.labels(
+                endpoint="/loyalty_plans/{loyalty_plan_id}", channel=self.channel_id, response_status=falcon.HTTP_404
+            ).inc()
             raise ResourceNotFoundError
 
         try:
@@ -434,6 +441,9 @@ class LoyaltyPlanHandler(BaseHandler, BaseLoyaltyPlanHandler):
             consents = self.db_session.execute(consent_query).all()
         except DatabaseError:
             api_logger.exception("Unable to fetch loyalty plan records from database")
+            loyalty_plan_get_counter.labels(
+                endpoint="/loyalty_plans/{loyalty_plan_id}", channel=self.channel_id, response_status=falcon.HTTP_500
+            ).inc()
             raise falcon.HTTPInternalServerError
 
         in_wallet_query = self.select_plan_ids_in_wallet_query.where(
@@ -462,11 +472,23 @@ class LoyaltyPlanHandler(BaseHandler, BaseLoyaltyPlanHandler):
         try:
             plan_information = self.db_session.execute(query).all()
         except DatabaseError:
+            loyalty_plan_get_counter.labels(
+                endpoint=" /loyalty_plans/{loyalty_plan_id}/journey_fields",
+                channel=self.channel_id,
+                response_status=falcon.HTTP_500,
+            ).inc()
             api_logger.error("Unable to fetch loyalty plan records from database")
+
             raise falcon.HTTPInternalServerError
 
         if not plan_information:
+            loyalty_plan_get_counter.labels(
+                endpoint=" /loyalty_plans/{loyalty_plan_id}/journey_fields",
+                channel=self.channel_id,
+                response_status=falcon.HTTP_404,
+            ).inc()
             api_logger.error("No loyalty plan information/credentials returned")
+
             raise ResourceNotFoundError
 
         schemes, creds, docs = list(zip(*plan_information))
@@ -672,8 +694,12 @@ class LoyaltyPlansHandler(BaseHandler, BaseLoyaltyPlanHandler):
             )
 
             consents = self.db_session.execute(consent_query).all()
+
         except DatabaseError:
-            api_logger.exception("Unable to fetch loyalty plan related records from database")
+            api_logger.exception("Unable to fetch loyalty plan records from database")
+            loyalty_plan_get_counter.labels(
+                endpoint="/loyalty_plans", channel=self.channel_id, response_status=falcon.HTTP_500
+            ).inc()
             raise falcon.HTTPInternalServerError
 
         try:
@@ -698,6 +724,10 @@ class LoyaltyPlansHandler(BaseHandler, BaseLoyaltyPlanHandler):
             schemes_and_images = self.db_session.execute(schemes_query).all()
 
         except DatabaseError:
+            loyalty_plan_get_counter.labels(
+                endpoint="/loyalty_plans_overview", channel=self.channel_id, response_status=falcon.HTTP_500
+            ).inc()
+
             api_logger.exception("Unable to fetch loyalty plan records from database")
             raise falcon.HTTPInternalServerError
 
