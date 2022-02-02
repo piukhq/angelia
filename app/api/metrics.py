c15bceadca9a79e137744a7b5c3fbdcc463e28c6
Tony Truong <ttruong@bink.com>
2022-02-02 09:30:24 +0000

    loy-1925-loyalty-card-monitoring

--- app/api/metrics.py
+++ app/api/metrics.py
@@ -4,3 +4,7 @@ from prometheus_client import Counter
 loyalty_plan_get_counter = Counter(
     "loyalty_plan_get_requests", "Total loyal plan get requests.", ["endpoint", "channel", "response_status"]
 )
+
+loyalty_card_counter = Counter(
+    "loyalty_card_requests", "Total loyal card requests.", ["endpoint", "channel", "response_status"]
+)
