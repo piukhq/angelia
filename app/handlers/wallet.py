e85225fc96a1d36495e7a5dca15667cc70e839be
MartfBink <mfranklin@bink.com>
2022-02-22 18:12:12 +0000

    stop processing if no card found, a little quicker

--- app/handlers/wallet.py
+++ app/handlers/wallet.py
@@ -320,6 +320,10 @@ class WalletHandler(BaseHandler):
 
         # query loyalty card info
         loyalty_card_result = self.query_scheme_accounts(schemeaccount_id=loyalty_card_id)
+        if len(loyalty_card_result)==0:
+            # no cards, stop any more processing 
+            return {}
+
         loyalty_card_index, loyalty_cards, join_cards = self.process_loyalty_cards_response(
             loyalty_card_result, full=True, overview=False
         )
