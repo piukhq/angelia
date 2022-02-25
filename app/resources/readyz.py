fb0df106de141165d511e217bd94fbdda6a3640d
MartfBink <mfranklin@bink.com>
2022-02-25 09:18:37 +0000

    clean up ready for integration

--- app/resources/readyz.py
+++ app/resources/readyz.py
@@ -1,7 +1,6 @@
 import falcon
 
 from app.api.auth import NoAuth
-from app.api.helpers.vault import get_azure_client
 from app.hermes.db import DB
 from app.messaging.message_broker import BaseMessaging
 from app.report import api_logger
@@ -15,24 +14,20 @@ class ReadyZ(Base):
     auth_class = NoAuth
 
     def on_get(self, req: falcon.Request, resp: falcon.Response) -> None:
-        self.errors = []
         pg = self._check_postgres()
         rb = self._check_rabbit()
-        az = self._check_secrets()
-        if pg and rb and az:
-            api_logger.info("Acording to ReadyZ; Angelia is all good.")
+        if pg and rb:
             resp.status = falcon.HTTP_204
         else:
-            api_logger.info("Acording to ReadyZ; Angelia is broken.")
-            api_logger.info("\n".join(self.errors))
-            resp.status = falcon.HTTP_404
+            resp.status = falcon.HTTP_503
+            resp.text = "Service Unavailable"
 
     def _check_postgres(self) -> bool:
         try:
             DB().engine.execute("SELECT 1").fetchone()
         except Exception as ex:
             healthy = False
-            self.errors.append(ex)
+            api_logger.error(ex)
         else:
             healthy = True
         return healthy
@@ -43,16 +38,6 @@ class ReadyZ(Base):
             conn.connect()
             available = conn.connected
         except Exception as ex:
-            self.errors.append(ex)
             available = False
-        return available
-
-    def _check_secrets(self) -> bool:
-        try:
-            secrets = get_azure_client()
-            secrets.list_properties_of_secrets()
-            available = True
-        except Exception as ex:
-            self.errors.append(ex)
-            available = False
+            api_logger.error(ex)
         return available
